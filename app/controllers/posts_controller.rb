682997bae1455fe491615bcc9995ffd605f7d800
1543rrr <73112533+1543rrr@users.noreply.github.com>
2021-03-29 23:09:39 +0900

    メッセージの削除機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[edit update destroy]
   def index
   end
 
@@ -22,11 +23,19 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destroy!
+    redirect_to root_path
   end
+
   private
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def set_post
+    @post = current_user.posts.find(params[:id])
+    redirect_to root_path, alert: "権限がありません"
   end
 
 end
